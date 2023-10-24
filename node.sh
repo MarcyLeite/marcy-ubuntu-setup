99569b0e986c99e346d38f71df10e3892436d74c
Matheus Marciano <39443406+MarcyLeite@users.noreply.github.com>
2023-10-24 18:48:54 +0000

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,6 +1,8 @@
 # node
 sudo curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o ~/Downloads/install_nvm.sh
-sudo bash ~/Downloads/install_nvm.sh
+mv ~/.bashrc ~/.bashrc-back
+bash ~/Downloads/install_nvm.sh
+mv ~/.bashrc-back ~/.bashrc
 
 source ~/.profile
 nvm install --lts
