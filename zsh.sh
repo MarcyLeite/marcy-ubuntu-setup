2d92f98699da0cf816fe9b917370f7dfc0f2298d
Matheus Marciano <39443406+MarcyLeite@users.noreply.github.com>
2023-10-24 18:45:03 +0000

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -2,11 +2,4 @@
 bash basics.sh
 bash git.sh
 sudo apt install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" << EOF
-Y
-setup.sh
-EOF
-
-cp ${PWD}/files/flower-dance.zsh-theme ~/.oh-my-zsh/themes/flower-dance.zsh-theme
-sed -i 's/ZSH_THEME=.*/ZSH_THEME="flower-dance"/g' ~/.zshrc
-chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
