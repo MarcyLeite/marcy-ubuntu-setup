de7ea060b46840579117c9b55ba27c162262ae2e
Matheus Marciano <39443406+MarcyLeite@users.noreply.github.com>
2023-10-24 18:27:57 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 bash basics.sh
-bash apps.sh
-bash git.sh
-
-bash monokai-gnome.sh
-bash nordic-gnome.sh
 bash zsh.sh
+bash nordic-gnome.sh
+bash monokai-gnome.sh
 
+bash git.sh
 bash node.sh
+
+bash apps.sh
